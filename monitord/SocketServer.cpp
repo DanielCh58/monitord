ee8c397181350e9c1ffdfba97701d81be9cb7adb
DanielCh58 <dc@vertigo.de>
2023-11-20 21:46:15 +0100

    fix: have to compare with nullptr in g++ 12

--- monitord/SocketServer.cpp
+++ monitord/SocketServer.cpp
@@ -754,7 +754,7 @@ void SocketThread::createSocket()
         	strncat(m_CommandBuffer,buffer,MAX_COMMANDLINE-strlen(m_CommandBuffer)-1) ;
 
 			/* Erst wenn mindestens eine Eingabezeile empfangen wurde, werden alle vollständigen Zeilen ausgewertet */
-			while ((posPtr=strstr(m_CommandBuffer,"\r\n"))>0)
+			while ((posPtr=strstr(m_CommandBuffer,"\r\n")) != nullptr)
 			{
 				/* Puffer nach dem CRLF zwischenspeichern */
 				char tempbuffer[MAX_COMMANDLINE] ;
